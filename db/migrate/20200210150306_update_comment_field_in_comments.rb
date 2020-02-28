0b7bef75a07a4efd1957a2871766bf79706faff6
yotsaphonSutweha <komsan_58@hotmail.com>
2020-02-28 18:43:35 +0000

    fix migrations

--- db/migrate/20200210150306_update_comment_field_in_comments.rb
+++ db/migrate/20200210150306_update_comment_field_in_comments.rb
@@ -1,6 +1,6 @@
 class UpdateCommentFieldInComments < ActiveRecord::Migration[5.2]
   def change
-    change_column  :comments, :comment, :text
-    change_column  :news_reports, :content, :text
+    # change_column  :comments, :comment, :text
+    # change_column  :news_reports, :content, :text
   end
 end
