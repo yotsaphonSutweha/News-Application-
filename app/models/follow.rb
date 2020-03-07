5d7227a395698b2462a786c4c8c3c93a29a0004d
yotsaphonSutweha <komsan_58@hotmail.com>
2020-03-07 17:07:02 +0000

    unit tested follows table

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,4 @@
 class Follow < ApplicationRecord
    belongs_to :profile
+   validates :followee_id, presence: true
 end
--- test/models/follow_test.rb
+++ test/models/follow_test.rb
@@ -4,4 +4,31 @@ class FollowTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # Happy path
+  test "Should save follow with a relationship to profile" do 
+    @profile = Profile.new(fname: profiles(:profile_writer).fname, sname: profiles(:profile_writer).sname, bio: profiles(:profile_writer).bio, role: profiles(:profile_writer).role, no_of_followers: profiles(:profile_writer).no_of_followers, user: @user)
+    @profile.save
+
+    @follow = Follow.new(followee_id: follows(:followee_one).id, profile:  @profile)
+
+    assert @follow.save
+  end
+  # End of Happy path
+
+  # Unhappy path
+  test "Should not save without a relationship to profile" do 
+    @follow = Follow.new(followee_id: follows(:followee_one).id, profile: nil)
+
+    assert_not @follow.save
+  end
+
+  test "Should not save without a followee_id" do 
+    @profile = Profile.new(fname: profiles(:profile_writer).fname, sname: profiles(:profile_writer).sname, bio: profiles(:profile_writer).bio, role: profiles(:profile_writer).role, no_of_followers: profiles(:profile_writer).no_of_followers, user: @user)
+    @profile.save
+
+    @follow = Follow.new(followee_id: '', profile:  @profile)
+    
+    assert_not @follow.save
+  end
+  # End of Unhappy path
 end
